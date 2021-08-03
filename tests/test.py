155891ea5ad7664fbdc1fedc19c457bc10344b36
Ava Dean <ava.dean@chem.ox.ac.uk>
2021-08-03 20:53:20 +0100

    Modifying test file

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
-from calc import Model, createCalculations, createDirectories, createSettings, createVariableSettings#, Setting
+from calc import Model, createCalculations, createDirectories, createSettings, createVariableSettings, Setting
 
+'''
 directories = createDirectories('halides', 'hyperfinebfield')
 settings = createSettings('hyperfine', 'soc')
 variableSettings = createVariableSettings('halides', 'hyperfinebfield')
@@ -8,10 +9,14 @@ calculations = createCalculations(*variableSettings,
                                   globalSettings=settings,
                                   directoryNames=directories,
                                   withDefaults=True)
+'''
 
-model = Model(calculations)
+#model = Model(calculations)
 #print(model)
+
+model = Model()
 
 model.create(force=True)
 
-model.run(force=True)
+model.sub(test=True)
+#model.sub()
