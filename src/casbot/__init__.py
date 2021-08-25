10073a4615520a46ca641d0021fdd1ded835bb13
Ava Dean <ava.dean@chem.ox.ac.uk>
2021-08-25 16:50:17 +0100

    Changed how Setting objects are created

--- src/casbot/__init__.py
+++ src/casbot/__init__.py
@@ -1,10 +1,10 @@
 from casbot.model import Model
 from casbot.calculation import Calculation, createCalculations, processCalculations
-from casbot.settings import Setting, createSettings, createVariableSettings
+from casbot.settings import getSetting, createSettings, createVariableSettings
 from casbot.data import createDirectories
 
 
 __all__ = ['Model',
            'Calculation', 'createCalculations', 'processCalculations',
-           'Setting', 'createSettings', 'createVariableSettings',
+           'getSetting', 'createSettings', 'createVariableSettings',
            'createDirectories']
